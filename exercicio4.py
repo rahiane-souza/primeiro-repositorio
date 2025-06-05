2b8500e3e248849652013c8816c201be225bb6af
rahiane-souza <rahiane.souza@escola.pr.gov.br>
2025-06-05 14:31:04 +0000

    exercicio11.py

---
+++ exercicio4.py
@@ -0,0 +1,8 @@
+#Crie um programa que receba o peso (kg) e a altura (m) de uma pessoa e calcule o IMC.
+#Classifique o resultado de acordo com a faixa
+
+#Abaixo do peso (< 18.5)
+Peso normal (18.5 a 24.9)
+#Sobrepeso (25 a 29.9)
+#Obesidade (>= 30)
+
